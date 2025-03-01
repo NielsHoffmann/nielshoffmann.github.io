7d4e90e10c5d10e1f0bd27ca1eb0221cef10dfee
Niels <niels.hoffmann@hotmail.com>
2025-03-01 20:01:41 +0100

    kva

---
+++ kva/kva.js
@@ -0,0 +1,28 @@
+ mydata = {
+    "name": "KVA",
+    "children": [
+        {
+            "name": "Eten en Drinken",
+            "children": [
+                {
+                    "name": "Eten"
+                },
+                {
+                    "name": "Drinken"
+                },
+                {
+                    "name": "Warm drinken"
+                },
+                {
+                    "name": "Noodrantsoen"
+                }
+            ]
+        },
+        {
+            "name": "Varen als groep"
+        },
+        {
+            "name": "Reddingen en incidenten"
+        }
+    ]
+}
